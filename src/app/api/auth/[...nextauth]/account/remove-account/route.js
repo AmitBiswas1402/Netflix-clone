5717f4a610e886407afb3b4318054ac025c391c0
Amit Biswas <amit.142biswas@gmail.com>
2024-03-12 21:47:16 +0530

    next auth, loader, model

---
+++ src/app/api/auth/[...nextauth]/account/remove-account/route.js
@@ -0,0 +1,15 @@
+import connectToDB from "@/database";
+import Account from "@/models/Account";
+import { NextResponse } from "next/server";
+
+export const dynamaic = "force-dynamic";
+
+export async function DELETE(req) {
+  try {
+  } catch (e) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+}
