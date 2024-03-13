1b5861d53d8d5be6702973c8e14f6e10a231791a
Amit Biswas <amit.142biswas@gmail.com>
2024-03-13 12:43:11 +0530

    accounts creation code

--- src/app/api/auth/[...nextauth]/account/create-account/route.js
+++ src/app/api/auth/[...nextauth]/account/create-account/route.js
@@ -6,6 +6,31 @@ export const dynamaic = "force-dynamic";
 
 export async function POST(req) {
   try {
+    await connectToDB()
+
+    const {name, pin, uid} = await req.json()
+
+    const isAccountAlreadyExists = await Account.find({uid, name})
+    const allAccounts = await Account.find({})
+
+    if(isAccountAlreadyExists){
+      return NextResponse.json({
+        success: false,
+        message: "Please try with a different name",
+      });
+    }
+
+    if(allAccounts && allAccounts.length === 4){
+      return NextResponse.json({
+        success: false,
+        message: "You can only add 4 accounts maximum",
+      });
+    }
+
+    const hashPin = await hash(pin, 12)
+
+
+
   } catch (e) {
     return NextResponse.json({
       success: false,
