d6d4ced3c8505ec3b4733c2b5c9d0a1a7976c4d8
Amit Biswas <amit.142biswas@gmail.com>
2024-03-14 12:47:37 +0530

    progress

--- src/components/manage-accounts/index.js
+++ src/components/manage-accounts/index.js
@@ -1,14 +1,26 @@
 "use client";
 
 import { GlobalContext } from "@/context";
-import { useContext } from "react";
+import { useSession } from "next-auth/react";
+import { useContext, useEffect } from "react";
 
 export default function ManageAccounts() {
     const {accounts, setAccounts} = useContext(GlobalContext)
+    const{data: session} = useSession()
 
     async function getAllAccounts(){
-        const res = await fetch('/api/account/get-all-accounts')      
+        const res = await fetch(`/api/account/get-all-accounts?id-${session?.user?.uid}`,{
+          method: "GET",
+        })      
+        const data = await res.json()
+
+        return data;
     }
+
+    useEffect(() => {
+      getAllAccounts()
+    }, [])
+
   return (
     <div className="min-h-screen flex justify-center flex-col items-center relative">
       <div className="flex justify-center flex-col items-center">
