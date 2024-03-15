3a87d6eed19b1de0c96d6972d6389604f476ed80
Amit Biswas <amit.142biswas@gmail.com>
2024-03-15 21:45:32 +0530

    big error f

--- src/context/index.js
+++ src/context/index.js
@@ -9,7 +9,7 @@ export const GlobalContext = createContext(null);
 export default function GlobalState({ children }) {
   const [loggedInAccount, setLoggedInAccount] = useState(null);
   const [accounts, setAccounts] = useState([]);
-  const [pageLoader, setPageLoader] = useState(true)
+  const [pageLoader, setPageLoader] = useState(true);
 
   const { data: session } = useSession();
 
@@ -17,7 +17,14 @@ export default function GlobalState({ children }) {
 
   return (
     <GlobalContext.Provider
-      value={{ loggedInAccount, setLoggedInAccount, accounts, setAccounts, pageLoader, setPageLoader }}
+      value={{
+        loggedInAccount,
+        setLoggedInAccount,
+        accounts,
+        setAccounts,
+        pageLoader,
+        setPageLoader,
+      }}
     >
       {children}
     </GlobalContext.Provider>
